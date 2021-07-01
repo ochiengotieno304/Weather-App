3ab45b33caca81867617311ea883636f122aa8ec
Ochieng Otieno <ochiengotieno71@gmail.com>
2021-07-01 10:47:43 +0300

    updated the weather state icon

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -23,10 +23,7 @@ class HomeController < ApplicationController
           end
 
           if item == 'icon'
-            icon_id = "#{content}"
-            icon_url = 'http://openweathermap.org/img/wn/'+ icon_id +'@2x.png'
-            @uri_1 = URI(icon_url)
-            @icon_response = Net::HTTP.get(@uri_1)
+            @icon_id = "#{content}"
           end
         end
         
